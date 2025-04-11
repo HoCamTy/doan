ee6e444a7b5cccbd7b16f8606a377deac3016d39
HoCamTy <hocamty@gmail.com>
2025-04-11 09:15:57 +0700

    Initial commit on login branch

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudUserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
+
